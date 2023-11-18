c42dd5cc9b4ce5736e28d80e10fef34f25a3a9c8
David Wong <11532851+davidgwong@users.noreply.github.com>
2023-11-17 21:27:32 -0800

    Added placeholders for commentRouter

--- postproject/routers/commentRouters.ts
+++ postproject/routers/commentRouters.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import * as database from "../controller/commentController";
 const router = express.Router();
-import { isLoggedIn } from "../utils/helperFunctions";
+import { ensureAuthenticated } from "../middleware/checkAuth";
+import { canEditComment, isLoggedIn } from "../utils/helperFunctions";
 
 router.get("/show/:commentid", async (req, res) => {
   const commentId = req.params.commentid;
@@ -9,11 +10,21 @@ router.get("/show/:commentid", async (req, res) => {
   const user = await req.user;
   const loggedIn = isLoggedIn(user);
   if (comment) {
-    const canEdit = comment.creator === user?.id || false;
+    const canEdit = canEditComment(comment.creator, user);
     res.render("individualComment", { comment, canEdit, loggedIn });
   } else {
     res.render("individualComment", { comment, loggedIn });
   }
 });
+
+router.post("/reply/:commentid", ensureAuthenticated, async (req, res) => {});
+
+router.get("/edit/:commentid", ensureAuthenticated, async (req, res) => {});
+
+router.post("/edit/:commentid", ensureAuthenticated, async (req, res) => {});
+
+router.get("/deleteconfirm/:commentid", ensureAuthenticated, async (req, res) => {});
+
+router.post("/delete/:commentid", ensureAuthenticated, async (req, res) => {});
 
 export default router;
--- postproject/utils/helperFunctions.ts
+++ postproject/utils/helperFunctions.ts
@@ -2,8 +2,12 @@ function canEditPost(postCreatorId: number, user: Express.User | undefined) {
   return postCreatorId === user?.id || false;
 }
 
+function canEditComment(commentCreatorId: number, user: Express.User | undefined) {
+  return  commentCreatorId === user?.id || false;
+}
+
 function isLoggedIn(user: Express.User | undefined) {
   return typeof user !== 'undefined' ? true : false;
 }
 
-export { canEditPost, isLoggedIn };
+export { canEditPost, canEditComment, isLoggedIn };
